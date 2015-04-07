1096b368004e544d07e69b380442f3254a69b92a
soledad89 <wangboshine@126.com>
2015-04-07 11:09:39 +0800

    add the test files

---
+++ test.cpp
@@ -0,0 +1,8 @@
+//To test the correctness of the machine_learning_algorithm
+#include "test.h"
+int main()
+{
+	printf("this algorithm works fine");
+	return 0;
+}
+
---
+++ test.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm.h>
+#include <vector.h>
+#include <iostream>
+using namespace std;
+
+#define MAXNUM 1024
+
